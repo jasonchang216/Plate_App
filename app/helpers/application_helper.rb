e02a44f3023a50accbd060ac03765784693a9964
Jason Chang <jasonchang216@gmail.com>
2017-07-02 21:40:01 -0400

    Star Rating Implementation
    - Created star rating in application helper
    - Rolled out stars to all rating tables and rearranged the tables
    - Asset precompiled

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  def render_stars(value)
+    output = ''
+    if value.nil?
+      output = ''
+    else (1..5).include?(value.floor)
+      value.floor.times { output += image_tag('star-on.png')}
+    end
+    output.html_safe
+  end
+
 end

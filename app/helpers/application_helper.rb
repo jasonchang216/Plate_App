201687d6e8b7980039402a2ee64db627f845a9f2
Jason Chang <jasonchang216@gmail.com>
2017-07-02 21:45:22 -0400

    Revert "Star fixing attempt"
    This reverts commit 6530216abddfc541d36e80e2673c65c9b92e8cf1.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
     if value.nil?
       output = ''
     else (1..5).include?(value.floor)
-      value.floor.times { output += image_tag('star-on-fd26bf0ea0990cfd808f7540f958eed324b86fc609bf56ec2b3a5612cdfde5f5.png')}
+      value.floor.times { output += image_tag('star-on.png')}
     end
     output.html_safe
   end
